65e34f0b042c4833c6d2ebac5a654e5bb3c288eb
Gabriel Odero <gabupostiga.gdco@gmail.com>
2022-05-28 02:28:42 +0300

    Registration response added

---
+++ WhatsappBusiness.CloudApi/Response/RegistrationResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace WhatsappBusiness.CloudApi.Response
+{
+    public class RegistrationResponse
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+    }
+}
