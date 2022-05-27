3c93015d64bfc3573326a9efda4f87fc303480ab
Gabriel Odero <gabupostiga.gdco@gmail.com>
2022-05-28 02:28:05 +0300

    Media upload response added

---
+++ WhatsappBusiness.CloudApi/Response/MediaUploadResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace WhatsappBusiness.CloudApi.Response
+{
+    public class MediaUploadResponse
+    {
+        [JsonProperty("id")]
+        public string MediaId { get; set; }
+    }
+}
