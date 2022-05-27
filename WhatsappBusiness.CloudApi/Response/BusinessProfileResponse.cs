32781ed15673202cf3869c9d3b166a0b27c32db0
Gabriel Odero <gabupostiga.gdco@gmail.com>
2022-05-28 02:27:50 +0300

    Business profile response added

---
+++ WhatsappBusiness.CloudApi/Response/BusinessProfileResponse.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+
+namespace WhatsappBusiness.CloudApi.Response
+{
+    public class BusinessProfileResponse
+    {
+        [JsonProperty("data")]
+        public List<BusinessProfileData> Data { get; set; }
+    }
+
+    public class BusinessProfileData
+    {
+        [JsonProperty("business_profile")]
+        public BusinessProfile BusinessProfile { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+    }
+
+    public partial class BusinessProfile
+    {
+        [JsonProperty("messaging_product")]
+        public string MessagingProduct { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("vertical")]
+        public string Vertical { get; set; }
+
+        [JsonProperty("about")]
+        public string About { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("websites")]
+        public List<Uri> Websites { get; set; }
+
+        [JsonProperty("profile_picture_url")]
+        public Uri ProfilePictureUrl { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+    }
+}
