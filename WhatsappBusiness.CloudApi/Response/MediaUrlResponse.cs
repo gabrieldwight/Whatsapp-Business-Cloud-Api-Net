45d2952715d636b44cc04ca6d10711c1243da186
Gabriel Odero <gabupostiga.gdco@gmail.com>
2022-05-28 02:28:26 +0300

    Media url response added

---
+++ WhatsappBusiness.CloudApi/Response/MediaUrlResponse.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace WhatsappBusiness.CloudApi.Response
+{
+    public class MediaUrlResponse
+    {
+        [JsonProperty("messaging_product")]
+        public string MessagingProduct { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("mime_type")]
+        public string MimeType { get; set; }
+
+        [JsonProperty("sha256")]
+        public string Sha256 { get; set; }
+
+        [JsonProperty("file_size")]
+        public string FileSize { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+    }
+}
