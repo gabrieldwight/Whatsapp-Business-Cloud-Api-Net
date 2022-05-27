e94292a5e5762f382e6a3e854f91ee6e7e0d723d
Gabriel Odero <gabupostiga.gdco@gmail.com>
2022-05-28 02:29:07 +0300

    Shared Whatsapp business account id response added

---
+++ WhatsappBusiness.CloudApi/Response/SharedWABAIDResponse.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+
+namespace WhatsappBusiness.CloudApi.Response
+{
+    public class SharedWABAIDResponse
+    {
+        [JsonProperty("data")]
+        public SharedWABAIDData Data { get; set; }
+    }
+
+    public class SharedWABAIDData
+    {
+        [JsonProperty("app_id")]
+        public string AppId { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("application")]
+        public string Application { get; set; }
+
+        [JsonProperty("data_access_expires_at")]
+        public long DataAccessExpiresAt { get; set; }
+
+        [JsonProperty("expires_at")]
+        public long ExpiresAt { get; set; }
+
+        [JsonProperty("is_valid")]
+        public bool IsValid { get; set; }
+
+        [JsonProperty("scopes")]
+        public List<string> Scopes { get; set; }
+
+        [JsonProperty("granular_scopes")]
+        public List<GranularScope> GranularScopes { get; set; }
+
+        [JsonProperty("user_id")]
+        public string UserId { get; set; }
+    }
+
+    public partial class GranularScope
+    {
+        [JsonProperty("scope")]
+        public string Scope { get; set; }
+
+        [JsonProperty("target_ids")]
+        public List<string> TargetIds { get; set; }
+    }
+}
