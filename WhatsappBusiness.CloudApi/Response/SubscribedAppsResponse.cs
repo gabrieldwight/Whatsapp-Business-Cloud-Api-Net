221ff638c2a95c18fd3d28ca71b3f2b3293a4560
Gabriel Odero <gabupostiga.gdco@gmail.com>
2022-05-28 22:18:10 +0300

    WABA subscribed apps response added

---
+++ WhatsappBusiness.CloudApi/Response/SubscribedAppsResponse.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace WhatsappBusiness.CloudApi.Response
+{
+    public class SubscribedAppsResponse
+    {
+        [JsonProperty("data")]
+        public List<SubscribedAppsData> Data { get; set; }
+    }
+
+    public class SubscribedAppsData
+    {
+        [JsonProperty("whatsapp_business_api_data")]
+        public WhatsappBusinessApiData WhatsappBusinessApiData { get; set; }
+    }
+
+    public partial class WhatsappBusinessApiData
+    {
+        [JsonProperty("link")]
+        public string Link { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+    }
+}
