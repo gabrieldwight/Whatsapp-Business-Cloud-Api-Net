7c2b1131c209d7d0bd32de01f663624c2b64d22a
Gabriel Odero <gabupostiga.gdco@gmail.com>
2022-05-29 02:50:59 +0300

    Whatsapp business cloud api config file added

---
+++ WhatsappBusiness.CloudApi/Configurations/WhatsAppBusinessCloudApiConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace WhatsappBusiness.CloudApi.Configurations
+{
+    public class WhatsAppBusinessCloudApiConfig
+    {
+        public string WhatsAppBusinessPhoneNumberId { get; set; }
+        public string WhatsAppBusinessAccountId { get; set; }
+        public string WhatsAppBusinessId { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
