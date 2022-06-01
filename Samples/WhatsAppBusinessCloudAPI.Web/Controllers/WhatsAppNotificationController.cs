9b046a41624ddd1a373208e58c9cd0294e6cd40f
Gabriel Odero <gabupostiga.gdco@gmail.com>
2022-06-01 03:01:04 +0300

    Updated webhook examples

--- Samples/WhatsAppBusinessCloudAPI.Web/Controllers/WhatsAppNotificationController.cs
+++ Samples/WhatsAppBusinessCloudAPI.Web/Controllers/WhatsAppNotificationController.cs
@@ -9,16 +9,38 @@ namespace WhatsAppBusinessCloudAPI.Web.Controllers
     public class WhatsAppNotificationController : ControllerBase
     {
         private readonly ILogger<WhatsAppNotificationController> _logger;
+        private string VerifyToken = "<YOUR VERIFY TOKEN STRING>";
+        private TextMessage textMessage;
+        private ImageMessage imageMessage;
 
         public WhatsAppNotificationController(ILogger<WhatsAppNotificationController> logger)
         {
             _logger = logger;
         }
 
-        [HttpPost("receive/TextMessage")]
-        public IActionResult ReceiveWhatsAppTextMessage([FromBody] TextMessageReceived textMessageReceived)
+        // Required step for configuring webhook to WhatsApp Cloud API
+        // Make sure the verifytoken matches with the hubverifytoken returned from whatsapp.
+        [HttpGet("receive/TextMessage")]
+        public ActionResult<string> ConfigureWhatsAppMessageWebhook([FromQuery(Name = "hub.mode")] string hubMode,
+                                                                    [FromQuery(Name = "hub.challenge")] int hubChallenge,
+                                                                    [FromQuery(Name = "hub.verify_token")] string hubVerifyToken)
         {
-            if (textMessageReceived is null)
+            _logger.LogInformation("Results Returned from WhatsApp Server\n");
+            _logger.LogInformation($"hub_mode={hubMode}\n");
+            _logger.LogInformation($"hub_challenge={hubChallenge}\n");
+            _logger.LogInformation($"hub_verify_token={hubVerifyToken}\n");
+
+            if (!hubVerifyToken.Equals(VerifyToken))
+            {
+                return Forbid("VerifyToken doesn't match");
+            }
+            return Ok(hubChallenge);
+        }
+
+        [HttpPost("receive/TextMessage")]
+        public IActionResult ReceiveWhatsAppTextMessage([FromBody] dynamic messageReceived)
+        {
+            if (messageReceived is null)
             {
                 return BadRequest(new
                 {
@@ -26,13 +48,33 @@ namespace WhatsAppBusinessCloudAPI.Web.Controllers
                 });
             }
 
-            // Handle logic to process WhatsApp Messages
-            _logger.LogInformation(JsonConvert.SerializeObject(textMessageReceived, Formatting.Indented));
+            
+            var messageType = Convert.ToString(messageReceived["entry"][0]["changes"][0]["value"]["messages"][0]["type"]);
 
-            return Ok(new
+            if (messageType.Equals("text"))
             {
-                Message = "Message is received"
-            });
+                var textMessageReceived = JsonConvert.DeserializeObject<TextMessageReceived>(Convert.ToString(messageReceived)) as TextMessageReceived;
+                textMessage = textMessageReceived.Entry.SelectMany(x => x.Changes).SelectMany(x => x.Value.Messages).FirstOrDefault();
+                _logger.LogInformation(JsonConvert.SerializeObject(textMessage, Formatting.Indented));
+
+                return Ok(new
+                {
+                    Message = "Text Message received"
+                });
+            }
+
+            if (messageType.Equals("image"))
+            {
+                var imageMessageReceived = JsonConvert.DeserializeObject<ImageMessageReceived>(Convert.ToString(messageReceived)) as ImageMessageReceived;
+                imageMessage = imageMessageReceived.Entry.SelectMany(x => x.Changes).SelectMany(x => x.Value.Messages).FirstOrDefault();
+                _logger.LogInformation(JsonConvert.SerializeObject(imageMessage, Formatting.Indented));
+
+                return Ok(new
+                {
+                    Message = "Image Message received"
+                });
+            }
+            return Ok();
         }
     }
 }
