770967d25a398437ea8811df3294615200832be3
Gabriel Odero <gabupostiga.gdco@gmail.com>
2022-08-22 00:43:38 +0300

    Added documentation for media upload

--- WhatsappBusiness.CloudApi/Media/Requests/UploadMediaRequest.cs
+++ WhatsappBusiness.CloudApi/Media/Requests/UploadMediaRequest.cs
@@ -7,9 +7,15 @@ namespace WhatsappBusiness.CloudApi.Media.Requests
         [JsonProperty("messaging_product")]
         public string MessagingProduct { get; private set; } = "whatsapp";
 
+        /// <summary>
+        /// Path to the file stored in your local directory. For example: "@/local/path/file.jpg".
+        /// </summary>
         [JsonProperty("file")]
         public string File { get; set; }
 
+        /// <summary>
+        /// Type of media file being uploaded.
+        /// </summary>
         [JsonProperty("type")]
         public string Type { get; set; }
     }
