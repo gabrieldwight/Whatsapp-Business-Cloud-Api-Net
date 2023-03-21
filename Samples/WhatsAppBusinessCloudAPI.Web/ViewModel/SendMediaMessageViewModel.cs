5728664272d5bc0d3e9f2c8bff088fb560fd3828
Gabriel Odero <gabupostiga.gdco@gmail.com>
2023-03-22 02:40:01 +0300

    Adding nullable property on sample model

--- Samples/WhatsAppBusinessCloudAPI.Web/ViewModel/SendMediaMessageViewModel.cs
+++ Samples/WhatsAppBusinessCloudAPI.Web/ViewModel/SendMediaMessageViewModel.cs
@@ -8,7 +8,7 @@ namespace WhatsAppBusinessCloudAPI.Web.ViewModel
         public List<SelectListItem> MediaType { get; set; }
         public string SelectedMediaType { get; set; }
         public string Message { get; set; }
-        public string MediaLink { get; set; }
-        public string MediaId { get; set; }
+        public string? MediaLink { get; set; }
+        public string? MediaId { get; set; }
     }
 }
