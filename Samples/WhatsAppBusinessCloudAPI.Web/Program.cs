9de49645fee9949318c66de0dbc994cf28822ea9
Gabriel Odero <gabupostiga.gdco@gmail.com>
2024-02-25 19:36:19 +0300

    Clean up on sample

---
+++ Samples/WhatsAppBusinessCloudAPI.Web/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using WhatsappBusiness.CloudApi.Configurations;
+using WhatsappBusiness.CloudApi.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews().AddNewtonsoftJson(options =>
+{
+	options.SerializerSettings.Formatting = Formatting.Indented;
+	options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+});
+
+builder.Services.Configure<WhatsAppBusinessCloudApiConfig>(options =>
+{
+	builder.Configuration.GetSection("WhatsAppBusinessCloudApiConfiguration").Bind(options);
+});
+
+WhatsAppBusinessCloudApiConfig whatsAppConfig = new WhatsAppBusinessCloudApiConfig();
+whatsAppConfig.WhatsAppBusinessPhoneNumberId = builder.Configuration.GetSection("WhatsApp")["WhatsAppBusinessPhoneNumberId"];
+whatsAppConfig.WhatsAppBusinessAccountId = builder.Configuration.GetSection("WhatsApp")["WhatsAppBusinessAccountId"];
+whatsAppConfig.WhatsAppBusinessId = builder.Configuration.GetSection("WhatsApp")["WhatsAppBusinessId"];
+whatsAppConfig.AccessToken = builder.Configuration.GetSection("WhatsApp")["AccessToken"];
+whatsAppConfig.AppName = builder.Configuration.GetSection("AppInfo")["AppName"];
+whatsAppConfig.Version = builder.Configuration.GetSection("AppInfo")["Version"];
+
+builder.Services.AddWhatsAppBusinessCloudApiService(whatsAppConfig);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
