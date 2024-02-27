054dd6d3b03b92b736ff89184c2766f11a38d943
theCJMan <thecjman.mostert@gmail.com>
2024-02-27 20:50:14 +0200

    Substantially increase functionality of BulkSend - Not Done

--- Samples/WhatsAppBusinessCloudAPI.Web/Controllers/BulkSendWhatsAppsController.cs
+++ Samples/WhatsAppBusinessCloudAPI.Web/Controllers/BulkSendWhatsAppsController.cs
@@ -10,6 +10,8 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 using WhatsappBusiness.CloudApi.Messages.Requests;
 using WhatsappBusiness.CloudApi.Response;
 using WhatsappBusiness.CloudApi.Webhook;
+using static System.Net.Mime.MediaTypeNames;
+using System;
 
 namespace WhatsAppBusinessCloudAPI.Web.Controllers
 {
@@ -18,14 +20,16 @@ namespace WhatsAppBusinessCloudAPI.Web.Controllers
         private readonly IWhatsAppBusinessClient _whatsAppBusinessClient;
 		private readonly ILogger<HomeController> _logger;		
 		private readonly IWebHostEnvironment _environment;
+        private readonly List<messageTypes> _msgTypes;
 
-        public record WUpContactRecord
+		public record WUpContactRecord
         {
 			public string Order { get; set; }
 			public string FirstName { get; set; }
 			public string LastName { get; set; }
 			public string Email { get; set; }
 			public string WupNum { get; set; }
+			public string MsgType { get; set; }
 			public string WupMsg { get; set; }
 			public string Template { get; set; }
 			public string Params { get; set; }
@@ -34,14 +38,25 @@ namespace WhatsAppBusinessCloudAPI.Web.Controllers
 			public string SendResult { get; set; }
 		}
 
+        private record messageTypes(string Name ,bool HasAtt );
+        
+        
 		public BulkSendWhatsAppsController(ILogger<HomeController> logger, IWhatsAppBusinessClient whatsAppBusinessClient, IWebHostEnvironment environment)
 		{
 			_logger = logger;
 			_whatsAppBusinessClient = whatsAppBusinessClient;			
 			_environment = environment;
+           
+            // These are the implemente Message Types that BulkSend can handle
+			_msgTypes = new List<messageTypes>();
+            _msgTypes.Add(new messageTypes("Audio", true));
+			_msgTypes.Add(new messageTypes("Doc - N/T", true));
+			_msgTypes.Add(new messageTypes("Image - N/T", true));
+            _msgTypes.Add(new messageTypes("Text - N/T", false));
+			_msgTypes.Add(new messageTypes("Video - N/T", true));
 		}
 
-        private WUpContactRecord ReplaceRecordData(WUpContactRecord record)
+		private WUpContactRecord ReplaceRecordData(WUpContactRecord record)
         {   // Go through each property value, and check if any of the replacement strings can be found, and replace it
 
 			string[] findArray = { "|FN|", "|LN|", "|Email|", "|WupNum|" };
@@ -82,6 +97,14 @@ namespace WhatsAppBusinessCloudAPI.Web.Controllers
 
 		}
 
+        /// <summary>
+        /// 1. Read the CSV file and go through each record
+        /// 2. Prep the record data
+        /// 3. Build the sendWhatsAppPayload so it can be used to send the WhatsApp
+        /// 4. Send the WhatsApp for this record, get the WAMID and store it in the SendResult
+        /// </summary>
+        /// <param name="fileInfo"></param>
+        /// <returns></returns>
         [HttpPost]
         [Route("[action]")]
         public async Task<ActionResult> ReadAndTraverseCSV(FileInfo fileInfo)
@@ -102,51 +125,73 @@ namespace WhatsAppBusinessCloudAPI.Web.Controllers
 
             foreach (DataRow row in dataTable.Rows)
             {
-                // Access individual fields of the record
+                // First test Key elements, if any error continue to next record
+                wUpContact.WupNum = sendMessageController.PrepNumber(row["WUp Num"].ToString());    // Prep the number and return the prepped number
+                if (wUpContact.WupNum == "-1")
+				{   // Error on the number, bounce to next record
+					row["SendResult"] = "ERROR: Not a WhatsApp number?";
+					continue; 
+                }
+
+				wUpContact.MsgType = row["MsgType"].ToString();
+				//if (!_msgTypes.Contains(wUpContact.MsgType))   
+                if (_msgTypes.Any(msgType => msgType.Name != wUpContact.MsgType))    
+				{   // MsgType is not in the MsgTypeImplemented list, bounce to next record
+					row["SendResult"] = "ERROR: Message Type not implemented";
+					continue;
+				}
+                
+                // Get rest of the Data for this record
                 // string order = row["Order"].ToString();
                 wUpContact.FirstName = row["First Name"].ToString();
 				wUpContact.LastName = row["Last Name"].ToString();
                 wUpContact.Email = row["Email"].ToString();
-				wUpContact.WupNum = sendMessageController.PrepNumber(row["WUp Num"].ToString());    // Prep the number and return the prepped number
-
-				wUpContact.WupMsg = row["WUp Msg"].ToString();
+                wUpContact.WupMsg = row["WUp Msg"].ToString();
                 wUpContact.Template = row["Template"].ToString();
                 wUpContact.Params = row["Params"].ToString();
 				wUpContact.WupAtt = row["Att"].ToString();
 				wUpContact.WupAttCap = row["WUp Att Cap"].ToString();
 				wUpContact.SendResult = row["SendResult"].ToString();
 
+                // Search and Replace any valid Placeholders
                 wUpContact = ReplaceRecordData(wUpContact);
 
-                sendWhatsAppPayload.Media = new WhatsAppMedia();
-				sendWhatsAppPayload.Media.Caption = wUpContact.WupAttCap;
+                // For the following Message Types there MUST be an attachement
+                if (_msgTypes.Any(msgType => msgType.Name == wUpContact.MsgType && msgType.HasAtt))
+                { 
 
-				// Build the unique list of attachments, if a new attachment is found, upload it and get the ID
-				if (uniqueAttWithMediaID.ContainsKey(wUpContact.WupAtt))
-                {
-                    sendWhatsAppPayload.Media.ID = uniqueAttWithMediaID[wUpContact.WupAtt]; // Retrieve the corresponding ID                                                                             
-                }
-                else
-                {
-                    // wUpAtt is not in the Dictionary, so we will upload the Media to Whatsapp and get the Media ID for use
-                    uploadMediaPayload.fileUploadMethod = "Normal";
-                    uploadMediaPayload.filePath = Path.Combine(_environment.WebRootPath, fileManagementController._localServerPaths.LocalFileUploadPath);
-                    uploadMediaPayload.fileName = wUpContact.WupAtt;
+CJM - Move all instance creation to outside of the loop
+                    sendWhatsAppPayload.Media = new WhatsAppMedia();
+				    sendWhatsAppPayload.Media.Caption = wUpContact.WupAttCap;
 
-                    FileInfo fileUploadedInfo = await fileManagementController.UploadFileToWhatsApp(uploadMediaPayload);                    
-
-                    if (uploadMediaPayload.fileUploadSuccess)
+				    // Build the unique list of attachments, if a new attachment is found, upload it and get the ID
+				    if (uniqueAttWithMediaID.ContainsKey(wUpContact.WupAtt))
                     {
-                        //File Uploaded successfully
-                        // string valueX = okResult?.Value?.ToString();
-                        sendWhatsAppPayload.Media.ID = uploadMediaPayload.fileWhatsAppID;
+                        sendWhatsAppPayload.Media.ID = uniqueAttWithMediaID[wUpContact.WupAtt]; // Retrieve the corresponding ID                                                                             
                     }
                     else
                     {
-                        sendWhatsAppPayload.Media.ID = "-1";
+                        // wUpAtt is not in the Dictionary, so we will upload the Media to Whatsapp and get the Media ID for use
+                        uploadMediaPayload.fileUploadMethod = "Normal";
+                        uploadMediaPayload.filePath = Path.Combine(_environment.WebRootPath, fileManagementController._localServerPaths.LocalFileUploadPath);
+                        uploadMediaPayload.fileName = wUpContact.WupAtt;
+
+                        FileInfo fileUploadedInfo = await fileManagementController.UploadFileToWhatsApp(uploadMediaPayload);                    
+
+                        if (uploadMediaPayload.fileUploadSuccess)
+                        {   //File Uploaded to WhatsApp successfully                     
+                            sendWhatsAppPayload.Media.ID = uploadMediaPayload.fileWhatsAppID;
+                        }
+                        else
+                        {   // The file was not uploaded to WhatsApp: This message cannot be sent, Skip to next record..
+						    row["SendResult"] = "ERROR: File could not be Uploaded to WhatsApp";
+						    continue;
+					    }
+                        uniqueAttWithMediaID.Add(wUpContact.WupAtt, sendWhatsAppPayload.Media.ID); // Add the entry to the Dictionary if not found
                     }
-                    uniqueAttWithMediaID.Add(wUpContact.WupAtt, sendWhatsAppPayload.Media.ID); // Add the entry to the Dictionary if not found
                 }
+                
+
 				
 				// Prep to send the WhatsApp
 				sendWhatsAppPayload.SendText = new SendTextPayload()
@@ -163,6 +208,8 @@ namespace WhatsAppBusinessCloudAPI.Web.Controllers
                     Name = wUpContact.Template,
                     Params = listParams
                 };
+
+
 
 				string WAMIds = sendMessageController.GetWAMId((await sendMessageController.SendTemplate_video_ParameterAsync(sendWhatsAppPayload)).Value);				
 
--- Samples/WhatsAppBusinessCloudAPI.Web/Controllers/SendMessageController.cs
+++ Samples/WhatsAppBusinessCloudAPI.Web/Controllers/SendMessageController.cs
@@ -96,17 +96,25 @@ namespace WhatsAppBusinessCloudAPI.Web.Controllers
 			return msgIDsBuilder.ToString();
 		}
 
-        [HttpPost]
+		/// <summary>
+		/// This is to prep the phone number to be in the correct length and form for whatsapp.
+        /// 1. If the Phone number is Null return -1
+        /// 2. Number must start with the country code assume South Africa (27)
+        /// 3. For South Africa the number must be minimum 9 digits without the country code and any leading zeros if less return -1
+        /// 4. If the number is more than 9 digits then assume the number is correct
+		/// </summary>
+		/// <param name="phoneNumber"></param>
+		/// <returns>String: a formated number for WhatsApp</returns>
+		[HttpPost]
         [Route("[action]")]
         public string PrepNumber(string phoneNumber)
         {
-            // This is to prep the phone number to be in the correct length and form for whatsapp
-            // Step 1: Strip out any character that is not a digit
-            string digitsOnly = new string(phoneNumber.Where(char.IsDigit).ToArray());
+			// Step 1: Strip out any character that is not a digit
+			string digitsOnly = phoneNumber?.Where(char.IsDigit).Any() == true ? new string(phoneNumber.Where(char.IsDigit).ToArray()) : "-1";
 
-            // Step 2: If the first character is a 0, then Remove it
-            if (digitsOnly.Length > 0 && digitsOnly[0] == '0')
-            {
+			// Step 2: If the first character is a 0, then Remove it
+			if (digitsOnly.Length > 9 && digitsOnly[0] == '0')
+            { // Remove any leading zero
                 digitsOnly = digitsOnly.Remove(0, 1);
             }
 
