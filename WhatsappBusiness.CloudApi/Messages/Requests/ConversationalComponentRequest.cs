36d5a80e4eff6f814351b8a3a80f44d815ac7f95
Gabriel Odero <gabupostiga.gdco@gmail.com>
2024-05-30 13:52:13 +0300

    Added conversational component support

---
+++ WhatsappBusiness.CloudApi/Messages/Requests/ConversationalComponentRequest.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace WhatsappBusiness.CloudApi.Messages.Requests
+{
+    public class ConversationalComponentRequest
+    {
+        [JsonProperty("enable_welcome_message")]
+        public bool EnableWelcomeMessage { get; set; }
+
+        [JsonProperty("commands")]
+        public List<ConversationalComponentCommand> Commands { get; set; }
+
+        [JsonProperty("prompts")]
+        public List<string> Prompts { get; set; }
+    }
+
+    public class ConversationalComponentCommand
+    {
+        [JsonProperty("command_name")]
+        public string CommandName { get; set; }
+
+        [JsonProperty("command_description")]
+        public string CommandDescription { get; set; }
+    }
+}
---
+++ WhatsappBusiness.CloudApi/Response/ConversationalComponentResponse.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace WhatsappBusiness.CloudApi.Response
+{
+    public class ConversationalComponentResponse
+    {
+        [JsonProperty("conversational_automation")]
+        public ConversationalAutomation ConversationalAutomation { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+    }
+
+    public class ConversationalAutomation
+    {
+        [JsonProperty("enable_welcome_message")]
+        public bool EnableWelcomeMessage { get; set; }
+
+        [JsonProperty("prompts")]
+        public List<string> Prompts { get; set; }
+
+        [JsonProperty("commands")]
+        public List<ConversationalCommand> Commands { get; set; }
+    }
+
+    public class ConversationalCommand
+    {
+        [JsonProperty("command_name")]
+        public string CommandName { get; set; }
+
+        [JsonProperty("command_description")]
+        public string CommandDescription { get; set; }
+    }
+}
