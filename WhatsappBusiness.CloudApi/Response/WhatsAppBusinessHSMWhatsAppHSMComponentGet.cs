80656218a2819b9bb63fa6afafea050c54f830d4
Taufik Hidayat <taufikdev88@gmail.com>
2024-08-21 17:19:56 +0700

    Added WhatsAppBusinessHSMWhatsAppHSMComponentGet

---
+++ WhatsappBusiness.CloudApi/Response/WhatsAppBusinessHSMWhatsAppHSMComponentGet.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace WhatsappBusiness.CloudApi.Response
+{
+    public class WhatsAppBusinessHSMWhatsAppHSMComponentGet
+    {
+        [JsonProperty("add_security_recommendation")]
+        public bool AddSecurityRecommendation { get; set; }
+
+        [JsonProperty("code_expiration_minutes")]
+        public int CodeExpirationMinutes { get; set; }
+
+        [JsonProperty("example")]
+        public object Example { get; set; }
+
+        [JsonProperty("format")]
+        public string Format { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+}
