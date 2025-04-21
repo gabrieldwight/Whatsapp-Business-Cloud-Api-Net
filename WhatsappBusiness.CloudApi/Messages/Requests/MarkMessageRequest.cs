69d7638cbf6dad09f867f6ad998d6ad2e2f0d7cb
Gabriel Odero <gabupostiga.gdco@gmail.com>
2025-04-21 23:06:16 +0300

    Fix #141

--- WhatsappBusiness.CloudApi/Messages/Requests/MarkMessageRequest.cs
+++ WhatsappBusiness.CloudApi/Messages/Requests/MarkMessageRequest.cs
@@ -2,15 +2,24 @@
 
 namespace WhatsappBusiness.CloudApi.Messages.Requests
 {
-    public class MarkMessageRequest
-    {
-        [JsonPropertyName("messaging_product")]
-        public string MessagingProduct { get; private set; } = "whatsapp";
+	public class MarkMessageRequest
+	{
+		[JsonPropertyName("messaging_product")]
+		public string MessagingProduct { get; private set; } = "whatsapp";
 
-        [JsonPropertyName("status")]
-        public string Status { get; set; }
+		[JsonPropertyName("status")]
+		public string Status { get; set; }
 
-        [JsonPropertyName("message_id")]
-        public string MessageId { get; set; }
-    }
+		[JsonPropertyName("message_id")]
+		public string MessageId { get; set; }
+
+		[JsonPropertyName("typing_indicator")]
+		public TypingIndicator TypingIndicator { get; set; }
+	}
+
+	public class TypingIndicator
+	{
+		[JsonPropertyName("type")]
+		public string Type { get; private set; } = "text";
+	}
 }
